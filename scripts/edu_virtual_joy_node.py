9f4d76fab2495b459baed91d84ef51614f6d018d
Stefan May <stefan.may@th-nuernberg.de>
2021-11-01 16:37:46 +0100

    Initial commit

---
+++ scripts/edu_virtual_joy_node.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from eduVirtualJoy import EduVirtualJoy
+
+if __name__ == '__main__':
+    rospy.init_node('edu_virtual_joy_node', anonymous=True)
+    EduVirtualJoy().run();
